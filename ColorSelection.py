337965ee0c850e1073d45f6d0fdcaf68f142200d
bobbybakes <bobby.a.baker@gmail.com>
2017-02-17 14:03:51 -0500

    Now Selecting by threshold

---
+++ ColorSelection.py
@@ -0,0 +1,32 @@
+import os
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpiImg
+import numpy as np
+
+image = mpiImg.imread('image/road3.png')
+print 'image {0}'.format('road.png')
+print 'image type {0}'.format(str(type(image)))
+print 'image shape {0}'.format(str(image.shape))
+
+xsize = image.shape[1]
+ysize = image.shape[0]
+
+print 'xsize {0}'.format(str(xsize))
+print 'ysize {0}'.format(str(ysize))
+
+colorSelect = np.copy(image)
+
+red = .5
+green = .5
+blue = .5
+rgb_threshold = [red, green, blue]
+
+thresholds = (image[:, :, 0] < rgb_threshold[0]) \
+             | (image[:, :, 1] < rgb_threshold[1]) \
+             | (image[:, :, 2] < rgb_threshold[2])
+
+colorSelect[thresholds] = [0, 0, 0]
+
+plt.imshow(colorSelect)
+plt.show()
