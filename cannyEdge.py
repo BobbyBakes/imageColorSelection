662aa43f70caf5e6ad175b66de0438b8183e60bd
bobbybakes <bobby.a.baker@gmail.com>
2017-02-21 14:34:09 -0500

    houghResults

--- cannyEdge.py
+++ cannyEdge.py
@@ -1,4 +1,4 @@
-#doing all the relevant imports
+# doing all the relevant imports
 import cv2
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
@@ -10,15 +10,16 @@ gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 # Define a kernel size for Gaussian smoothing / blurring
 # Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
-kernel_size = 3
-blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
+kernel_size = 303
+blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
 blur_grayCopy = np.uint8(blur_gray)
 
 # Define parameters for Canny and run it
 # NOTE: if you try running this code you might want to change these!
-low_threshold = 0
-high_threshold = 1000000
+low_threshold = 50
+high_threshold = 150
 edges = cv2.Canny(blur_grayCopy, low_threshold, high_threshold)
+print edges
 
 # Display the image
 plt.imshow(edges, cmap='Greys_r')
